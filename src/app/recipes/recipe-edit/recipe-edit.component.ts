06bcb08569b7e6b3ab91517afb5815a95db2ddb7
WillZhangFly <oliverottawa@gmail.com>
2021-11-11 11:06:15 -0500

    nested reactive form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipe.service';
 
@@ -29,19 +29,38 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  get controls() { // a getter!
+    console.log("form controls:::", (<FormArray>this.recipeForm.get('ingredients')).controls)
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   private initForm() {
     let recipeName = '', recipeImagePath = '', recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if(this.editMode){
-      const {name, imagePath, description} = this.recipeService.getRecipe(this.id);
+      const recipe = this.recipeService.getRecipe(this.id);
+      const {name, imagePath, description} = recipe;
       recipeName = name;
       recipeImagePath = imagePath;
       recipeDescription = description;
+
+      if(recipe['ingredients']){
+        for( const {name , amount} of recipe.ingredients){
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(name),
+              'amount': new FormControl(amount)
+            })
+          )
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     });
   }
 
