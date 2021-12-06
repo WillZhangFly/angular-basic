43e091685efa886ab3f557a04681c31ca97fffc2
WillZhangFly <oliverottawa@gmail.com>
2021-12-06 16:43:50 -0500

    sign up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,19 +1,37 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
-    selector:'app-auth',
-    templateUrl:'./auth.component.html'
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
 })
-export class AuthComponent{
-    isLoginMode = true;
+export class AuthComponent {
+  isLoginMode = true;
 
-    onSwitchMode() {
-        this.isLoginMode = !this.isLoginMode;
+  constructor(private auth: AuthService) {}
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const { email, password } = form.value;
+
+    if (!this.isLoginMode) {
+      this.auth.signup(email, password).subscribe(
+        (resData) => {
+          console.log('resData ', resData);
+        },
+        (error) => {
+          console.log('Error ', error);
+        }
+      );
     }
 
-    onSubmit(form : NgForm){
-        console.log(form.value);
-        form.reset();
-    }
+    form.reset();
+  }
 }
