29f22168ddee6a742e6778347f4b687803ff1ded
WillZhangFly <oliverottawa@gmail.com>
2022-01-07 16:06:47 -0500

    auth module seperate

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthComponent } from './auth.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
+    SharedModule
+  ],
+})
+export class AuthModule {}
